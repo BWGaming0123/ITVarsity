d610cee62bee6ab6ee71258d0a94803f5641060b
Michael Homonnay <michaelhomonnay5@gmail.com>
2025-07-02 15:15:49 +0200

    Week 7 Lesson 3

--- Week 7/strings.py
+++ Week 7/strings.py
@@ -1,6 +1,19 @@
-#strings
+#Advanced concepts - Strings
 
-message = """Bob's World
-is cool"""
+message = 'Hello World! '
 
-print(message)
+print(message[0])
+print(message[1])
+
+print(message[-1])
+
+print(len(message))
+
+print(message.strip()) #Removes whitespace from the beginning and end of the string
+print(message.lower()) #Converts the string to lowercase
+print(message.split(',')) #Splits the string into a list based on commas
+
+#upper method
+print(message.upper()) #Converts the string to uppercase
+#replace method
+print(message.replace('World', 'Python')) #Replaces 'World' with 'Python'
