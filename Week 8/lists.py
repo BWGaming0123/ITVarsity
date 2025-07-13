566aaa39e6fb7a0961836afdfb04baeee26fc7c1
Michael Homonnay <michaelhomonnay5@gmail.com>
2025-07-13 11:36:04 +0200

    Week 8 Lesson 2

--- Week 8/lists.py
+++ Week 8/lists.py
@@ -4,3 +4,17 @@ print(fruits[0])  # Output: apple
 
 fruits[1] = "blueberry"  # Change the second item
 print(fruits)  # Output: ['apple', 'blueberry', 'cherry']
+
+fruits = ["apple", "banana", "cherry"]
+
+fruits.append("kiwi")
+print(fruits)
+
+fruits.insert(1, "orange")  # Insert at index 1
+print(fruits)  
+
+fruits.remove("kiwi")
+print(fruits)
+
+fruits.sort(reverse=True)
+print(fruits)  
