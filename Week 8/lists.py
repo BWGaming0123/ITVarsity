4a21b04edf508fea5150a8689748c92ebac1dc1d
Michael Homonnay <michaelhomonnay5@gmail.com>
2025-07-13 11:12:12 +0200

    Week 8 Lesson 1

---
+++ Week 8/lists.py
@@ -0,0 +1,6 @@
+fruits = ["apple", "banana", "cherry"]
+
+print(fruits[0])  # Output: apple
+
+fruits[1] = "blueberry"  # Change the second item
+print(fruits)  # Output: ['apple', 'blueberry', 'cherry']
