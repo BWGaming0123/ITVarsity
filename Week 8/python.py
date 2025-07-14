d49bff32ce116c3785397c80568a868e310d3762
Michael Homonnay <michaelhomonnay5@gmail.com>
2025-07-14 11:09:27 +0200

    Week 8 Lesson 7

---
+++ Week 8/python.py
@@ -0,0 +1,20 @@
+# Operators
+
+# Addition (+)
+# Subtraction (-)
+# Multiplication (*)
+# Division (/)
+# Modulus (%)
+# Exponentiation (**)
+
+x = 10
+y = 2
+
+print(x + y)  # Output: 12
+print(x - y)  # Output: 8
+print(x * y)  # Output: 20
+print(x / y)  # Output: 5.0
+print(x % y)  # Output: 0
+print(x ** y)  # Output: 100
+
+x += 2
