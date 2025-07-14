50873889c6735bcd923c17e9200e485e71acee33
Michael Homonnay <michaelhomonnay5@gmail.com>
2025-07-14 11:05:04 +0200

    Week 8 Lesson 5

---
+++ Week 8/dictionary.py
@@ -0,0 +1,8 @@
+my_dict = {'apple': 3, 'banana': 5, 'cherry': 2}
+
+print(my_dict['apple'])  # Output: 3
+
+my_dict['grape'] = 4  # Change the value for 'grape'
+print(my_dict)  # Output: {'apple': 3, 'banana': 5, 'cherry': 2, 'grape': 4}
+
+my_dict['banana'] = 7  # Change the value for 'banana'
