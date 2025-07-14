2d3cba4289740163526d8adde64f6b15efa6824e
Michael Homonnay <michaelhomonnay5@gmail.com>
2025-07-14 10:59:11 +0200

    Week 8 Lesson 4

--- Week 8/lists.py
+++ Week 8/lists.py
@@ -18,3 +18,29 @@ print(fruits)
 
 fruits.sort(reverse=True)
 print(fruits)  
+
+#Sets
+'''
+my_set = {1, 2, 3, 4, 5}
+
+print(my_set)  # Output: {1, 2, 3, 4, 5}
+
+my_set.add(6)
+print(my_set)  # Output: {1, 2, 3, 4, 5, 6}
+
+my_set.remove(3)
+print(my_set)  # Output: {1, 2, 4, 5, 6}
+'''
+set1 = {1, 2, 3}
+set2 = {3, 4, 5}
+
+# Union
+union_set  = set1.union(set2)
+print(union_set)  # Output: {1, 2, 3, 4, 5}
+# Intersection
+inter_set = set1.intersection(set2)
+print(inter_set)  # Output: {3}
+
+# Difference
+diff_set = set1.difference(set2)
+print(diff_set)  # Output: {1, 2}
